7573d60a1dd6743c88b8aab120af6f7a46b9f889
JagadeeswaranSampath <108391237+JagadeeswaranSampath@users.noreply.github.com>
2022-07-31 23:59:07 +0530

    Add files via upload

---
+++ objcom.js
@@ -0,0 +1,19 @@
+var obj1 = { name: "Person 1", age:5 };
+var obj2 = { age:5, name: "Person 1" };
+
+//console.log(JSON.stringify(obj1) === JSON.stringify(obj2));  false
+
+const ordered1={};
+const ordered2={};
+
+Object.keys(obj1).sort().forEach(key => {
+    ordered1[key]= obj1[key];
+});
+
+Object.keys(obj2).sort().forEach(key => {
+    ordered2[key]= obj2[key];
+});
+//console.log(ordered1);
+//console.log(ordered2);
+
+console.log(JSON.stringify(ordered1) === JSON.stringify(ordered2));
